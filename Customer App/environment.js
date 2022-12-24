699331739216dbed8b15fcdc6d0c8e3ddbf6bc16
sharan <sharan.gohar@gmail.com>
2022-12-24 16:38:24 +0500

    clean up

--- Customer App/environment.js
+++ Customer App/environment.js
@@ -11,12 +11,12 @@ const ENV = {
     WS_GRAPHQL_URL: 'ws://192.168.100.90:8000/graphql',
     SERVER_URL: 'http://192.168.100.90:8000/', // put / at the end of server url
     IOS_CLIENT_ID_GOOGLE:
-      '967541328677-uq7f7odvmeea2pb2sq0l7q320ds86536.apps.googleusercontent.com',
+      '',
     ANDROID_CLIENT_ID_GOOGLE:
-      '967541328677-u9lbhmiesp67j3md9b8nk6mkhooeljur.apps.googleusercontent.com',
+      '',
     FACEBOOK_APP_ID: '404956210315749',
-    AMPLITUDE_API_KEY: '358ef0deb443586e2cc4ddd4380151f0',
-    STRIPE_PUBLIC_KEY: 'pk_test_lEaBbVGnTkzja2FyFiNlbqtw',
+    AMPLITUDE_API_KEY: '',
+    STRIPE_PUBLIC_KEY: '',
     STRIPE_IMAGE_URL: 'http://192.168.100.90:8000/assets/images/logo.png',
     STRIPE_STORE_NAME: 'Enatega'
   },
@@ -25,12 +25,12 @@ const ENV = {
     WS_GRAPHQL_URL: 'wss://staging-enatega-single-api.herokuapp.com/graphql',
     SERVER_URL: 'https://staging-enatega-single-api.herokuapp.com/', // put / at the end of server url
     IOS_CLIENT_ID_GOOGLE:
-      '967541328677-nf8h4ou7rhmq9fahs87p057rggo95eah.apps.googleusercontent.com',
+      '',
     ANDROID_CLIENT_ID_GOOGLE:
-      '967541328677-7264tf7tkdtoufk844rck9mimrve135c.apps.googleusercontent.com',
+      '',
     FACEBOOK_APP_ID: '404956210315749',
-    AMPLITUDE_API_KEY: '358ef0deb443586e2cc4ddd4380151f0',
-    STRIPE_PUBLIC_KEY: 'pk_test_lEaBbVGnTkzja2FyFiNlbqtw',
+    AMPLITUDE_API_KEY: '',
+    STRIPE_PUBLIC_KEY: '',
     STRIPE_IMAGE_URL:
       'https://staging-enatega-single-api.herokuapp.com/assets/images/logo.png',
     STRIPE_STORE_NAME: 'Enatega'
@@ -40,12 +40,12 @@ const ENV = {
     WS_GRAPHQL_URL: 'wss://prod-enatega-single-api.herokuapp.com/graphql',
     SERVER_URL: 'https://prod-enatega-single-api.herokuapp.com/', // put / at the end of server url
     IOS_CLIENT_ID_GOOGLE:
-      '94983896797-irt6u2cmq2sjcp7j1rj9m9pqptjd12ue.apps.googleusercontent.com',
+      '',
     ANDROID_CLIENT_ID_GOOGLE:
-      '94983896797-omp6mi32rl3126siafgnskrqql0rdk5c.apps.googleusercontent.com',
+      '',
     FACEBOOK_APP_ID: '3017447961609878',
-    AMPLITUDE_API_KEY: '358ef0deb443586e2cc4ddd4380151f0',
-    STRIPE_PUBLIC_KEY: 'pk_test_lEaBbVGnTkzja2FyFiNlbqtw',
+    AMPLITUDE_API_KEY: '',
+    STRIPE_PUBLIC_KEY: '',
     STRIPE_IMAGE_URL:
       'https://prod-enatega-single-api.herokuapp.com/assets/images/logo.png',
     STRIPE_STORE_NAME: 'Enatega'
